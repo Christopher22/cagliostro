f677a40464ac6eb4c3b0a8f170039446af469009
Christopher Gundler <christopher@gundler.de>
2020-06-27 23:05:53 +0200

    Fix regular expression in corner cases

--- cagliostro/model/Responses.cpp
+++ cagliostro/model/Responses.cpp
@@ -18,15 +18,15 @@ namespace cagliostro::model {
 Responses *Responses::load(const QString &output, QString subject, QObject *parent) {
   auto *file = new QFile(output);
   if (!file->open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append) || !file->seek(0)) {
-	file->deleteLater();
-	return nullptr;
+    file->deleteLater();
+    return nullptr;
   }
 
   return new Responses(file, std::move(subject), parent);
 }
 
 Responses::Responses(QIODevice *file, QString subject, QObject *parent)
-	: QObject(parent), output_(file), subject_(std::move(subject)), num_responses_(0) {
+    : QObject(parent), output_(file), subject_(std::move(subject)), num_responses_(0) {
   assert(file != nullptr && file->isOpen());
   file->seek(0);
   file->setParent(this);
@@ -34,8 +34,9 @@ Responses::Responses(QIODevice *file, QString subject, QObject *parent)
 }
 
 bool Responses::contains(QStringView page_name) const noexcept {
-  return !this->findChildren<Response *>(QRegularExpression(QString("%1\\.*").arg(page_name)),
-										 Qt::FindDirectChildrenOnly).isEmpty();
+  const auto children = this->findChildren<Response *>(QRegularExpression(QString("%1\\/.+").arg(page_name)),
+                                                       Qt::FindDirectChildrenOnly);
+  return !children.isEmpty();
 }
 
 bool Responses::contains(Question *question) const noexcept {
@@ -48,36 +49,36 @@ bool Responses::contains(Page *page) const noexcept {
 
 void Responses::parse() {
   if (!output_.atEnd()) {
-	QString current_line;
-	bool first_line = true;
-	while (output_.readLineInto(&current_line)) {
-	  if (first_line) {
-		first_line = false;
-		continue;
-	  }
+    QString current_line;
+    bool first_line = true;
+    while (output_.readLineInto(&current_line)) {
+      if (first_line) {
+        first_line = false;
+        continue;
+      }
 
-	  // Skip empty lines
-	  if (current_line.trimmed().size() == 0) {
-		continue;
-	  }
+      // Skip empty lines
+      if (current_line.trimmed().size() == 0) {
+        continue;
+      }
 
-	  // Try to parse a given response
-	  if (Response::create(current_line, num_responses_++, this) == nullptr) {
-		qWarning() << "Unable to parse saved response: " << current_line;
-	  }
-	}
+      // Try to parse a given response
+      if (Response::create(current_line, num_responses_++, this) == nullptr) {
+        qWarning() << "Unable to parse saved response: " << current_line;
+      }
+    }
   }
 }
 
 bool Responses::record(Question *question) {
   if (!question->isAnswered() || this->contains(question)) {
-	return false;
+    return false;
   }
 
   // Write the header, if not already written
   if (output_.device()->size() == 0) {
-	const auto *parent = this->parent();
-	output_ << QString("DATETIME\tSUBJECT\tQUESTION (%1)\tANSWER\n").arg(parent != nullptr ? parent->objectName() : "");
+    const auto *parent = this->parent();
+    output_ << QString("DATETIME\tSUBJECT\tQUESTION (%1)\tANSWER\n").arg(parent != nullptr ? parent->objectName() : "");
   }
 
   auto *response = new Response(question, num_responses_++, this);
@@ -90,7 +91,7 @@ bool Responses::record(Page *page) {
   bool result = true;
   const auto questions = page->questions();
   for (auto *i = questions.begin(), *end = questions.end(); i < end && result; ++i) {
-	result = this->record(*i);
+    result = this->record(*i);
   }
   return result;
 }
@@ -102,27 +103,27 @@ QVector<Responses::Response *> Responses::responses() noexcept {
 Responses::Response *Responses::Response::create(const QString &line, int index, Responses *parent) {
   const auto elements = line.split('\t', Qt::SkipEmptyParts);
   if (elements.size() != 4) {
-	return nullptr;
+    return nullptr;
   }
 
   QDateTime date = QDateTime::fromString(elements[0], Qt::ISODate);
   if (!date.isValid()) {
-	return nullptr;
+    return nullptr;
   }
 
   return new Response(index, date, elements[2], elements[3], parent);
 }
 
 Responses::Response::Response(int index, QDateTime creation, const QString &name, QString answer, Responses *parent)
-	: Entity(name, index, parent), creation_(std::move(creation)), answer_(std::move(answer)) {
+    : Entity(name, index, parent), creation_(std::move(creation)), answer_(std::move(answer)) {
   assert(parent != nullptr);
   this->setObjectName(name);
 }
 
 Responses::Response::Response(Question *question, int index, Responses *parent)
-	: Entity(question->fullName(), index, parent),
-	  creation_(QDateTime::currentDateTime()),
-	  answer_(question->answer()) {
+    : Entity(question->fullName(), index, parent),
+      creation_(QDateTime::currentDateTime()),
+      answer_(question->answer()) {
   assert(parent != nullptr);
   assert(question != nullptr);
   assert(question->isAnswered());
